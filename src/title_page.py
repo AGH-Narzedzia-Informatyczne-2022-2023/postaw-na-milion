c0ebaf5f98d1ab156282df52ba06ca4678f864b4
Gabi363 <g.zmuda@novitas.com.pl>
2022-11-21 14:48:26 +0100

    add multiple screens handling

---
+++ src/title_page.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+
+class TitlePage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        # self.create_button()
+        # self.parent = parent
+        btn = Button(
+            self,
+            text='Zacznij grę !',
+            bd='3',
+            command=controller.start_quiz,
+            width=10,
+            height=3,
+        )
+        btn.pack(side="top")
+        #btn.place(x=700, y=450)
+
+    # def create_button(self):
+    #     btn = Button(
+    #         self.parent,
+    #         text='Zacznij grę !',
+    #         bd='3',
+    #         command=self.parent.destroy,
+    #         width=10,
+    #         height=3,
+    #     )
+    #     # btn.pack(side="top")
+    #     btn.place(x=700, y=450)
